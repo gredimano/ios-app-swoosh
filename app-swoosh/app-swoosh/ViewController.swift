590557a8d0340e66f85197fa269925b1c055467c
Generoso Redimano Jr <gredimano@objectbright.com>
2018-02-25 13:26:22 +0800

    Add Segues.

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -3,9 +3,13 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var swoosh: UIImageView!
     @IBOutlet weak var bgImg: UIImageView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+
     }
 }
 

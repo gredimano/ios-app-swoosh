73fdc43f939cb0e6f9f1ca9c8a17df06a8167cc6
Generoso Redimano Jr <gredimano@objectbright.com>
2018-02-26 10:32:54 +0800

    Pass data to VC.

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -2,11 +2,44 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    var player: Player!
+
+    @IBOutlet weak var nextBtn: BorderButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        player = Player()
     }
 
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
+    }
+
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague("mens")
+    }
+
+
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague("womens")
+    }
+
+    @IBAction func onCoedsTapped(_ sender: Any) {
+        selectLeague("coed")
+    }
+
+    func selectLeague(_ leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
+    
+    @IBAction func unwindFromLeagueVC(unwindSegue: UIStoryboardSegue) {
+        
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let skillVC = segue.destination as? SkillVC {
+            skillVC.player = player
+        }
     }
 }
--- app-swoosh/app-swoosh/Controller/SkillVC.swift
+++ app-swoosh/app-swoosh/Controller/SkillVC.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class SkillVC: UIViewController {
 
+    var player: Player!
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
